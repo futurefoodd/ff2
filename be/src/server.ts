3ffde841df35344ac6cd86f519fce1edb1ef81fc
Azrar <85500300+pancake-ctrl@users.noreply.github.com>
2026-09-13 23:00:06 +0800

    Revert "Merge pull request #1 from futurefoodd/chore/github-actions"
    This reverts commit 35843bf196dfdfa1105ae4151a011b660b9e0991.

--- be/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from './app.js';
-import { env } from './env.js';
-
-const app = createApp();
-
-app.listen(env.port, () => {
-  console.log(`BFF listening on ${env.baseUrl}`);
-});
