f4db0dccc9268c964b766edf38d02357cc5d08a0
futurefoodd <consult@futurefoods.com.my>
2026-09-13 23:46:27 +0800

    Chore/GitHub actions (#6)
    * chore/Added vitest-coverage - test setup
    
    * Add staging branch to CI workflow triggers
    
    * chore: update CI workflow for Docker image tagging and publishing; add .nvmrc and Dockerfile
    
    * chore: update package.json and package-lock.json; add tsc-alias and update test scripts
    
    * chore: comment out Stripe payment tests in CI workflow
    
    * chore: add error handling middleware and request ID generation; update routes and configuration
    
    * chore: update Dockerfile to change CMD to use server.js instead of index.js
    
    * chore: remove unused jest-dom import from test setup
    
    * force CI retry
    
    ---------
    
    Co-authored-by: Azrar <azrar2277@gmail.com>
    Co-authored-by: Azrar <85500300+pancake-ctrl@users.noreply.github.com>

--- be/src/routes/apiStripe.ts
+++ be/src/routes/apiStripe.ts
@@ -1,17 +1,16 @@
 import { Router } from "express"
-// import { HttpError } from "../middleware/errorHandler.js"
 import type { Request, Response } from "express"
-import { asyncHandler } from "@utils/asyncHandler.js"
 import { StripeClient } from "@client/stripeClient.js"
 import {env} from "../env.js"
+// import { HttpError } from "../middleware/errorHandler.js"
 
 export const stripeRouter = Router()
 
-const BASE_URL = env.baseUrl;
+const BASE_URL = env.baseUrl
 
 stripeRouter.post(
   "/create-checkout-session",
-  asyncHandler(async (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const session = await StripeClient.checkout.sessions.create({
       line_items: [
         {
@@ -27,5 +26,5 @@ stripeRouter.post(
     })
 
     res.redirect(303, session.url?? '')
-  })
+  }
 )
